de3a9f2f89d7120e55206b1669e05206fac18603
Manoj-Pal-21 <manojpal211999@gmail.com>
2022-06-21 23:12:00 +0530

    added button alert

--- Input/input.js
+++ Input/input.js
@@ -3,4 +3,9 @@
 const ele = document.querySelector("#input");
 ele.addEventListener("input", (event) => {
     console.log(event.target.value);
-});
+}); 
+
+const btn = document.querySelector("#click");
+btn.addEventListener("click", () => {
+    alert("Hello Manoj How Are U")
+})
